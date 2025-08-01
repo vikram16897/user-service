03dff72a6d49f1f6698ca5c4cbbef5a511bbb073
vikram16897 <vkt16897@gmail.com>
2025-08-01 21:00:06 +0530

    added test file to check

---
+++ test/add_test.go
@@ -0,0 +1,12 @@
+package test
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	expected := 5
+
+	if result != expected {
+		t.Errorf("Expected %d, but got %d", expected, result)
+	}
+}
